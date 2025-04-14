6878bf644177eb6da111c771e671189fd75546e6
DylanPho <159408670+DylanPho@users.noreply.github.com>
2025-04-14 15:39:51 -0600

    updated movies.js

--- Movies.js
+++ Movies.js
@@ -4,17 +4,27 @@ var Schema = mongoose.Schema;
 mongoose.connect(process.env.DB);
 
 // Movie schema
-var MovieSchema = new Schema({
+const MovieSchema = new mongoose.Schema({
     title: { type: String, required: true },
-    releaseDate: Number,
-    genre: String,
-    actors: [
-        {
-            actorName: String,
-            characterName: String
-        }
-    ]
-});
+    releaseDate: {
+      type: Number,
+      required: true,
+      min: [1900, 'Must be after 1899'],
+      max: [2100, 'Must be before 2100']
+    },
+    genre: {
+      type: String,
+      required: true,
+      enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction']
+    },
+    actors: {
+      type: [{
+        actorName: String,
+        characterName: String
+      }],
+      required: [true, 'Actors field is required']
+    }
+  });
 
 // return the model
 module.exports = mongoose.model('Movie', MovieSchema);
