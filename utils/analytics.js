4a1918421bd9ba7bc3612f9dcb89c9d38d3efd2d
DylanPho <159408670+DylanPho@users.noreply.github.com>
2025-04-20 20:03:38 -0600

    updated mongodb connection string

--- utils/analytics.js
+++ utils/analytics.js
@@ -1,31 +1,27 @@
 const rp = require('request-promise');
-const crypto = require('crypto');
-
 const GA_TRACKING_ID = process.env.GA_TRACKING_ID;
 
-function trackReviewEvent(movieTitle) {
-    const clientId = crypto.randomBytes(16).toString("hex");
+function trackAnalytics(category, action, label, value, dimension, metric) {
+  const options = {
+    method: 'GET',
+    uri: 'https://www.google-analytics.com/collect',
+    qs: {
+      v: '1',
+      tid: GA_TRACKING_ID,
+      cid: Math.floor(Math.random() * 1000000000),
+      t: 'event',
+      ec: category,
+      ea: action,
+      el: label,
+      ev: value,
+      cd1: dimension,
+      cm1: metric,
+    },
+    headers: {
+      'Cache-Control': 'no-cache'
+    }
+  };
 
-    const options = {
-        method: 'GET',
-        uri: 'https://www.google-analytics.com/collect',
-        qs: {
-            v: '1', // API version
-            tid: GA_TRACKING_ID,
-            cid: clientId,
-            t: 'event',
-            ec: 'Feedback', // Event Category
-            ea: 'Rating',   // Event Action
-            el: 'API Request for Movie Review', // Event Label
-            cd1: movieTitle, // Custom Dimension 1: Movie Title
-            cm1: 1           // Custom Metric 1: One review added
-        },
-        headers: {
-            'Cache-Control': 'no-cache'
-        }
-    };
-
-    return rp(options);
+  return rp(options);
 }
-
-module.exports = { trackReviewEvent };
+module.exports = { trackAnalytics };
